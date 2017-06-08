1cb2791e939ecf293f1a7a5c4829df4c8c0a41b9
bchuang <boris855161@gmail.com>
2017-06-08 23:23:51 +0800

    Initial SolutionProject

---
+++ TestWebSite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Test(RoomList roomList)
+        {
+            var json = JsonConvert.SerializeObject(
+                   roomList, Formatting.Indented,
+                   new JsonSerializerSettings
+                   {
+                       ContractResolver = new CamelCasePropertyNamesContractResolver()
+                   });
+            return new JsonResult() { Data = json };
+        }
+    }
+}
